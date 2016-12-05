950ea043f37e8ae9da0f84bafdede5e9386d32b4
ramanallamilli <rama.nallamilli@gmail.com>
2016-12-05 23:37:11 +0000

    proto

--- aero-worker/src/main/scala/org/rntech/worker/Worker.scala
+++ aero-worker/src/main/scala/org/rntech/worker/Worker.scala
@@ -1,24 +1,54 @@
 package org.rntech.worker
 
-//TODO should see if we can find away to extract from Byte array into default formater (this should also probably use some kind of compresion format e.g kyro)
-case class Context(data: Array[Byte], headers: Map[String, Any])
+import scala.pickling.Defaults._
+import scala.pickling.binary._
+import scala.util.{Failure, Success, Try}
 
-abstract class Processor[In,Out] {
+case class ProcessorFailed(msg: String, cause: Throwable) extends RuntimeException(cause)
+
+trait Deserailizer {
+  def desez(data: Array[Byte]): AnyRef
+}
+
+trait Serializer {
+  def sez[T](a: T): Array[Byte]
+}
+
+object PicklingDeserailizer extends Deserailizer {
+  def desez(data: Array[Byte]): AnyRef = BinaryPickleArray(data).unpickle[AnyRef]
+}
+
+object PicklingSerializer extends Serializer {
+  def sez[T](a: T): Array[Byte] = a.pickle.value
+}
+
+abstract class Processor[In, Out] {
+
+  def processors[A, B](fn: A => B)
+
+
+
   def process(input: In, headers: Map[String, Any]): Out
 
-  protected[this] def run(data: Array[Byte], headers: Map[String, Any]) = {
-    import scala.pickling.Defaults._
-    import scala.pickling.binary._
-    //https://github.com/scala/pickling
-    //TODO switch out so can choose any pickle implementation
-    val pick = BinaryPickleArray(data)
-    process(pick.unpickle[AnyRef].asInstanceOf[In], Map.empty[String,Any])
+  protected[this] def run(data: Array[Byte], headers: Map[String, Any])(implicit sez: Serializer, desez: Deserailizer) = {
+    //TODO switch out so can choose any parsing implementation?
+    val pick = desez.desez(data)
+    Try(process(pick.asInstanceOf[In], Map.empty[String, Any])) match {
+      case Success(result) =>
+        sendOutputData(sez.sez(result))
+      case Failure(ex) =>
+        reportFailure(s"Failure during processing, ${getClass.getSimpleName}", ex)
+    }
+  }
 
+  def sendOutputData(data: Array[Byte]) = {
 
   }
 
-  def validateIn: Class[In]
-  def validateOut: Class[Out]
+  def reportFailure(msg: String, ex: Throwable) = {
+
+  }
+
 }
 
 trait Fetcher[Out] {
