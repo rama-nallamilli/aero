c277c5c42cd5839b3e88bded1bf36b7d98a26a3b
rama-nallamilli <rama.nallamilli@gmail.com>
2016-12-09 16:39:32 +0000

    proto

--- aero-worker/src/main/scala/org/rntech/jobs/JobListener.scala
+++ aero-worker/src/main/scala/org/rntech/jobs/JobListener.scala
@@ -3,12 +3,46 @@ package org.rntech.jobs
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
+import akka.http.scaladsl.model.ws.{BinaryMessage, Message, TextMessage}
 import akka.http.scaladsl.server.Directives._
 import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.{Flow, Sink, Source}
+import akka.util.ByteString
+
 import scala.io.StdIn
 class JobListener {
 
 
+
+}
+
+object WebsocketTest extends App {
+  implicit val system = ActorSystem("my-system")
+  implicit val materializer = ActorMaterializer()
+  // needed for the future flatMap/onComplete in the end
+  implicit val executionContext = system.dispatcher
+
+  def greeter: Flow[Message, Message, Any] =
+    Flow[Message].mapConcat {
+      case tm: TextMessage =>
+        TextMessage(Source.single("Hello ") ++ tm.textStream ++ Source.single("!")) :: Nil
+      case bm: BinaryMessage =>
+        // ignore binary messages but drain content to avoid the stream being clogged
+        bm.dataStream.runWith(Sink.ignore)
+        Nil
+    }
+  val websocketRoute =
+    path("greeter") {
+      handleWebSocketMessages(greeter)
+    }
+
+  val bindingFuture = Http().bindAndHandle(websocketRoute, "localhost", 8080)
+
+  println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
+  StdIn.readLine() // let it run until user presses return
+  bindingFuture
+    .flatMap(_.unbind()) // trigger unbinding from the port
+    .onComplete(_ => system.terminate()) // and shutdown when done
 
 }
 
