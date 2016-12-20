074ec25e618040f59d1e71f1b87383b369a258da
rama-nallamilli <rama.nallamilli@gmail.com>
2016-12-20 16:25:59 +0000

    proto

--- aero-worker/src/main/scala/org/rntech/consul/Registrator.scala
+++ aero-worker/src/main/scala/org/rntech/consul/Registrator.scala
@@ -9,14 +9,46 @@ case class RegistratorConfig(consulHttpUrl: String)
 
 class Registrator(consulHttpUrl: String) {
 
-  def registerSelf(implicit system: ActorSystem, materializer: ActorMaterializer) = {
+  def registerSelf(registrationId: String, name: String, tags: Set[String])(implicit system: ActorSystem, materializer: ActorMaterializer) = {
     val registrationEndpoint = s"$consulHttpUrl/v1/agent/service/register"
 
     //https://www.consul.io/docs/agent/http/agent.html#agent_service_register
+    import org.json4s._
+    import org.json4s.JsonDSL._
+
+    val json = ("ID" -> registrationId) ~
+      ("Name" -> name) ~
+      ("Tags" -> tags) ~
+      ("Address" -> "TODO") ~
+      ("Port" -> "TODO") ~
+      ("Check" -> (
+        ("HTTP" -> "http://localhost:8080/health") ~
+          ("DeregisterCriticalServiceAfter" -> "90m") ~
+          ("Interval" -> "10s")))
+
     val responseFuture = Http().singleRequest(HttpRequest(uri = "http://akka.io")
       .withMethod(HttpMethods.PUT)
-     //   .withEntity()
+      //   .withEntity()
     )
+
+    //    {
+    //      "ID": "redis1",
+    //      "Name": "redis",
+    //      "Tags": [
+    //      "primary",
+    //      "v1"
+    //      ],
+    //      "Address": "127.0.0.1",
+    //      "Port": 8000,
+    //      "EnableTagOverride": false,
+    //      "Check": {
+    //        "DeregisterCriticalServiceAfter": "90m",
+    //        "Script": "/usr/local/bin/check_redis.py",
+    //        "HTTP": "http://localhost:5000/health",
+    //        "Interval": "10s",
+    //        "TTL": "15s"
+    //      }
+    //    }
 
 
   }
