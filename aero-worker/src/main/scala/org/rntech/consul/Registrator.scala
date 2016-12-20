a4b7ee050ed9e1daa6789f7d76fd492caac80ad0
rama-nallamilli <rama.nallamilli@gmail.com>
2016-12-20 16:14:31 +0000

    proto

--- aero-worker/src/main/scala/org/rntech/consul/Registrator.scala
+++ aero-worker/src/main/scala/org/rntech/consul/Registrator.scala
@@ -1,5 +1,23 @@
 package org.rntech.consul
 
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.{HttpHeader, HttpMethods, HttpRequest}
+import akka.stream.ActorMaterializer
+
+case class RegistratorConfig(consulHttpUrl: String)
+
 class Registrator(consulHttpUrl: String) {
 
+  def registerSelf(implicit system: ActorSystem, materializer: ActorMaterializer) = {
+    val registrationEndpoint = s"$consulHttpUrl/v1/agent/service/register"
+
+    //https://www.consul.io/docs/agent/http/agent.html#agent_service_register
+    val responseFuture = Http().singleRequest(HttpRequest(uri = "http://akka.io")
+      .withMethod(HttpMethods.PUT)
+     //   .withEntity()
+    )
+
+
+  }
 }
