bda176979e32a1caa7cf07501a013ffee4f15ae4
rama-nallamilli <rama.nallamilli@gmail.com>
2016-12-23 15:10:28 +0000

    proto

--- aero-worker/src/test/scala/feature/ConsulRegistrationFeature.scala
+++ aero-worker/src/test/scala/feature/ConsulRegistrationFeature.scala
@@ -1,12 +1,17 @@
 package feature
 
-import org.scalatest.{DoNotDiscover, FeatureSpec}
+import org.scalatest.{DoNotDiscover, FeatureSpec, GivenWhenThen}
 
 @DoNotDiscover
-class ConsulRegistrationFeature extends FeatureSpec {
+class ConsulRegistrationFeature extends FeatureSpec with GivenWhenThen {
 
   feature("Worker registration against Consul cluster") {
     scenario("On startup the worker successfully registers with the Consul cluster") {
+      Given("the application is running and consul is available")
+
+      When("it starts up")
+
+      Then("it should have registered itself with Consul")
 
     }
 
