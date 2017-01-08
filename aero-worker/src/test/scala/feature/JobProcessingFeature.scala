65cddd9487bee74e0bf2a7c9668b1329dccd7d74
ramanallamilli <rama.nallamilli@gmail.com>
2017-01-08 14:45:51 +0000

    initial

--- aero-worker/src/test/scala/feature/JobProcessingFeature.scala
+++ aero-worker/src/test/scala/feature/JobProcessingFeature.scala
@@ -6,6 +6,15 @@ import org.scalatest.{DoNotDiscover, FeatureSpec}
 class JobProcessingFeature extends FeatureSpec {
 
   feature("Processes inbound jobs") {
-    scenario("Successfully processes inbound jobs") {}
+
+    // before this can happen the flow must have been submitted and loaded so we know what the current code to run is for the job
+    // and the next one to send it too
+//    1. recieve job
+//    2. process job
+//    3. send next job to next availble worker (use consul to get the address of the worker that the job needs to go to)
+//    4. the hash key is used to target the node
+    scenario("Successfully processes inbound jobs") {
+
+    }
   }
 }
